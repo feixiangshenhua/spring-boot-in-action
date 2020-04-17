94694de21b7cdc5a43f4840a80423b2abc1d9f40
zongweibai <jonezw@163.com>
2020-04-17 21:09:30 +0800

    fix build script issues

--- docker/build.sh
+++ docker/build.sh
@@ -13,4 +13,9 @@ sudo docker build -t file-server:1.0.0 .
 # Docker部署
 mkdir -p /home/zongwei/app_data/file_server/data
 mkdir -p /home/zongwei/app_data/file_server/log
+
+# 停止并删除旧的部署
+sudo docker stop file_server
+sudo docker rm file_server
+
 sudo docker run --name file_server --restart unless-stopped -p 8899:8080 -v /home/zongwei/app_data/file_server/data:/usr/local/modules/File-Server/data -v /home/zongwei/app_data/file_server/log:/usr/local/modules/File-Server/log -v /etc/timezone:/etc/timezone -v /etc/localtime:/etc/localtime -d file-server:1.0.0
